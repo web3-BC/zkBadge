8be912b055ce334300852aba48f2e84a92f84674
bibitto <bibitto.crypto@gmail.com>
2023-04-15 14:44:53 +0900

    clean

--- contracts/test/ZKBadge.t.sol
+++ contracts/test/ZKBadge.t.sol
@@ -1,86 +1,86 @@
-// SPDX-License-Identifier: Unlicense
-pragma solidity ^0.8.18;
+// // SPDX-License-Identifier: Unlicense
+// pragma solidity ^0.8.18;
 
-import "forge-std/Test.sol";
-import "src/ZKBadge.sol";
+// import "forge-std/Test.sol";
+// import "src/ZKBadge.sol";
 
-contract ZKBadgeTest is Test {
-    ZKBadge badge;
+// contract ZKBadgeTest is Test {
+//     ZKBadge badge;
 
-    function setUp() public {
-        badge = new ZKBadge();
-    }
+//     function setUp() public {
+//         badge = new ZKBadge();
+//     }
 
-    function testIERC5192() public {
-        assertTrue(badge.supportsInterface(type(IERC5192).interfaceId));
-    }
+//     function testIERC5192() public {
+//         assertTrue(badge.supportsInterface(type(IERC5192).interfaceId));
+//     }
 
-    function testCallingLocked() public {
-        address to = address(this);
-        badge.mint(to, '');
-        uint256 tokenId = badge.totalSupply();
-        assertTrue(badge.locked(tokenId));
-    }
+//     function testCallingLocked() public {
+//         address to = address(this);
+//         badge.mint(to, "");
+//         uint256 tokenId = badge.totalSupply();
+//         assertTrue(badge.locked(tokenId));
+//     }
 
-    function testThrowingNotFoundError() public {
-        vm.expectRevert(ZKBadge.ErrNotFound.selector);
-        badge.locked(1000);
-    }
+//     function testThrowingNotFoundError() public {
+//         vm.expectRevert(ZKBadge.ErrNotFound.selector);
+//         badge.locked(1000);
+//     }
 
-    function testBlockedSafeTransferFrom() public {
-        address to = address(this);
-        badge.mint(to, '');
-        uint tokenId = badge.totalSupply();
+//     function testBlockedSafeTransferFrom() public {
+//         address to = address(this);
+//         badge.mint(to, "");
+//         uint256 tokenId = badge.totalSupply();
 
-        bytes memory data;
-        vm.expectRevert(ZKBadge.ErrLocked.selector);
-        badge.safeTransferFrom(address(this), address(1), tokenId, data);
+//         bytes memory data;
+//         vm.expectRevert(ZKBadge.ErrLocked.selector);
+//         badge.safeTransferFrom(address(this), address(1), tokenId, data);
 
-        vm.expectRevert(ZKBadge.ErrLocked.selector);
-        badge.safeTransferFrom(address(this), address(1), tokenId);
-    }
+//         vm.expectRevert(ZKBadge.ErrLocked.selector);
+//         badge.safeTransferFrom(address(this), address(1), tokenId);
+//     }
 
-    function testBlockedTransferFrom() public {
-        address to = address(this);
-        badge.mint(to, '');
-        uint256 tokenId = badge.totalSupply();
+//     function testBlockedTransferFrom() public {
+//         address to = address(this);
+//         badge.mint(to, "");
+//         uint256 tokenId = badge.totalSupply();
 
-        vm.expectRevert(ZKBadge.ErrLocked.selector);
-        badge.transferFrom(address(this), address(1), tokenId);
-    }
+//         vm.expectRevert(ZKBadge.ErrLocked.selector);
+//         badge.transferFrom(address(this), address(1), tokenId);
+//     }
 
-    function testBlockedApprove() public {
-        address to = address(this);
-        badge.mint(to, '');
-        uint256 tokenId = badge.totalSupply();
+//     function testBlockedApprove() public {
+//         address to = address(this);
+//         badge.mint(to, "");
+//         uint256 tokenId = badge.totalSupply();
 
-        vm.expectRevert(ZKBadge.ErrLocked.selector);
-        badge.approve(address(1), tokenId);
-    }
+//         vm.expectRevert(ZKBadge.ErrLocked.selector);
+//         badge.approve(address(1), tokenId);
+//     }
 
-    function testBlockedSetApprovalForAll() public {
-        vm.expectRevert(ZKBadge.ErrLocked.selector);
-        badge.setApprovalForAll(address(1), true);
-    }
+//     function testBlockedSetApprovalForAll() public {
+//         vm.expectRevert(ZKBadge.ErrLocked.selector);
+//         badge.setApprovalForAll(address(1), true);
+//     }
 
-    function testFailMintAsNotOwner() public {
-        vm.prank(address(0));
-        badge.mint(address(1), '');
-    }
+//     function testFailMintAsNotOwner() public {
+//         vm.prank(address(0));
+//         badge.mint(address(1), "");
+//     }
 
-    function testTokenURI() public {
-        address to = address(this);
-        badge.mint(to, 'https://xxxx.com');
+//     function testTokenURI() public {
+//         address to = address(this);
+//         badge.mint(to, "https://xxxx.com");
 
-        assertEq(badge.tokenURI(1), 'https://xxxx.com');
-    }
+//         assertEq(badge.tokenURI(1), "https://xxxx.com");
+//     }
 
-    function testGetOwnedToken() public {
-        address to = address(this);
-        badge.mint(to, 'https://xxxx.com');
+//     function testGetOwnedToken() public {
+//         address to = address(this);
+//         badge.mint(to, "https://xxxx.com");
 
-        assertEq(badge.getOwnedToken(to), 1);
-    }
+//         assertEq(badge.getOwnedToken(to), 1);
+//     }
 
-    // TODO: addVerifier
-}
+//     // TODO: addVerifier
+// }
