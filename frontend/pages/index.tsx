1cddf668726184782d4a05a83d62658a1fd6b687
Asuma Yamada <yamadaasuma@gmail.com>
2023-04-15 21:13:28 +0900

    feat: add logo on header

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,5 +1,5 @@
 import { QRCode } from "react-qr-svg";
-import { Box, Typography } from "@mui/material";
+import { AppBar, Box, Toolbar, Typography } from "@mui/material";
 import Image from "next/image";
 import {
   qrTwitterProofRequestJson,
@@ -8,46 +8,59 @@ import {
 
 export default function Home() {
   return (
-    <Box textAlign={"center"} p={4} height={"100vh"}>
-      <Typography variant="h2">Claim our zkBadges!</Typography>
-
-      <Box display={"flex"} justifyContent={"center"} my={4}>
-        <Box mr={5}>
-          <Box display={"flex"} justifyContent={"center"}>
-            <Image width={50} height={50} src="/twitterLogo.png" alt="" />
-          </Box>
-          <Typography variant="body1" my={3}>
-            you must have more than 40 twitter followers !
-          </Typography>
-          <Box display={"flex"} justifyContent={"center"}>
-            <QRCode
-              level="Q"
-              style={{ width: 256 }}
-              value={JSON.stringify(qrTwitterProofRequestJson)}
-            />
-          </Box>
-        </Box>
-        <Box>
-          <Box display={"flex"} justifyContent={"center"}>
-            <Image
-              width={50}
-              height={50}
-              src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Instagram_logo_2022.svg/1024px-Instagram_logo_2022.svg.png"
+    <>
+      <AppBar position="static" sx={{ backgroundColor: "#854ee6" }}>
+        <Toolbar>
+          <Box maxWidth={100} height={64} alignItems={'center'} display={'flex'}>
+            <img
+              src="/logo.png"
               alt=""
+              style={{ objectFit: "contain", height: 140 }}
             />
           </Box>
-          <Typography variant="body1" my={3}>
-            you must have more than 30 instagram followers !
-          </Typography>
-          <Box display={"flex"} justifyContent={"center"}>
-            <QRCode
-              level="Q"
-              style={{ width: 256 }}
-              value={JSON.stringify(qrInstagramProofRequestJson)}
-            />
+        </Toolbar>
+      </AppBar>
+      <Box textAlign={"center"} p={4} height={"100vh"}>
+        <Typography variant="h2">Claim our zkBadges!</Typography>
+
+        <Box display={"flex"} justifyContent={"center"} my={4}>
+          <Box mr={5}>
+            <Box display={"flex"} justifyContent={"center"}>
+              <Image width={50} height={50} src="/twitterLogo.png" alt="" />
+            </Box>
+            <Typography variant="body1" my={3}>
+              you must have more than 40 twitter followers !
+            </Typography>
+            <Box display={"flex"} justifyContent={"center"}>
+              <QRCode
+                level="Q"
+                style={{ width: 256 }}
+                value={JSON.stringify(qrTwitterProofRequestJson)}
+              />
+            </Box>
+          </Box>
+          <Box>
+            <Box display={"flex"} justifyContent={"center"}>
+              <Image
+                width={50}
+                height={50}
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Instagram_logo_2022.svg/1024px-Instagram_logo_2022.svg.png"
+                alt=""
+              />
+            </Box>
+            <Typography variant="body1" my={3}>
+              you must have more than 30 instagram followers !
+            </Typography>
+            <Box display={"flex"} justifyContent={"center"}>
+              <QRCode
+                level="Q"
+                style={{ width: 256 }}
+                value={JSON.stringify(qrInstagramProofRequestJson)}
+              />
+            </Box>
           </Box>
         </Box>
       </Box>
-    </Box>
+    </>
   );
 }
