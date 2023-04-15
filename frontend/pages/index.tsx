3069ea868f18692fceb454d8f91ba7b55e6d3582
Asuma Yamada <yamadaasuma@gmail.com>
2023-04-15 14:04:52 +0900

    fix: add lib

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,124 +1,99 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import { QRCode } from "react-qr-svg";
+import { Box } from "@mui/material";
 
-const inter = Inter({ subsets: ['latin'] })
+const styles = {
+  root: {
+    color: "#2C1752",
+    fontFamily: "sans-serif",
+    textAlign: "center",
+  },
+  title: {
+    color: "#7B3FE4",
+  },
+};
 
+// update with your contract address
+const deployedContractAddress = "0x44f53039FA2d6B2f397ad34aB5aff2c0828560B3";
+
+// more info on query based requests: https://0xpolygonid.github.io/tutorials/wallet/proof-generation/types-of-auth-requests-and-proofs/#query-based-request
+// qrValueProofRequestExample: https://github.com/0xPolygonID/tutorial-examples/blob/main/on-chain-verification/qrValueProofRequestExample.json
+const qrProofRequestJson = {
+  id: "7f38a193-0918-4a48-9fac-36adfdb8b542",
+  typ: "application/iden3comm-plain-json",
+  type: "https://iden3-communication.io/proofs/1.0/contract-invoke-request",
+  thid: "7f38a193-0918-4a48-9fac-36adfdb8b542",
+  body: {
+    reason: "airdrop participation",
+    transaction_data: {
+      contract_address: deployedContractAddress,
+      method_id: "b68967e2",
+      chain_id: 80001,
+      network: "polygon-mumbai",
+    },
+    scope: [
+      {
+        id: 1,
+        circuitId: "credentialAtomicQuerySigV2OnChain",
+        query: {
+          allowedIssuers: ["*"],
+          context:
+            "https://raw.githubusercontent.com/0xPolygonID/tutorial-examples/main/credential-schema/proof-of-dao-longevity.jsonld",
+          credentialSubject: {
+            entryDate: {
+              $lt: 20230415,
+            },
+          },
+          type: "ProofOfDaoLongevity",
+        },
+      },
+    ],
+  },
+};
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <Box sx={styles.root}>
+      <h2 style={styles.title}>Claim an ERC20 zk airdrop on Polygon Mumbai</h2>
+      <p>
+        Age verification: You must prove your date of birth was before Jan 1,
+        2001 to claim.
+      </p>
+      <p>
+        Complete age verification by issuing yourself a Polygon ID claim via{" "}
+        <a
+          href="https://polygontechnology.notion.site/Issue-yourself-a-KYC-Age-Credential-claim-a06a6fe048c34115a3d22d7d1ea315ea"
+          target="_blank"
+        >
+          KYC Age Credentials
+        </a>{" "}
+        then scan QR code within Polygon ID app to claim tokens
+      </p>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+      <Box sx={{display: "flex", justifyContent: "center"}} my={4}>
+        <QRCode
+          level="Q"
+          style={{ width: 256 }}
+          value={JSON.stringify(qrProofRequestJson)}
         />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
+      </Box>
+      <br />
+      <p>
+        Github:{" "}
         <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          href="https://github.com/oceans404/tutorial-examples/tree/main/on-chain-verification"
           target="_blank"
-          rel="noopener noreferrer"
         >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Find in-depth information about Next.js features and API.
-          </p>
+          On-chain verification tutorial
         </a>
-
+      </p>
+      <p>
+        Polygonscan:{" "}
         <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          href={`https://mumbai.polygonscan.com/token/${deployedContractAddress}`}
           target="_blank"
-          rel="noopener noreferrer"
         >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
+          Token ERC20zkAirdrop
         </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+      </p>
+    </Box>
+  );
 }
